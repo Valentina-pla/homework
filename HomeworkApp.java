842285b79d03da6675f7703d1403dd214a68e857
valentina <v.platonova16051991@gmail.com>
2021-08-17 12:21:35 +0300

    dz

---
+++ HomeworkApp.java
@@ -0,0 +1,59 @@
+package lesson1;
+
+public class HomeworkApp {
+
+
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Apple");
+        System.out.println("Banana");
+
+        checkSumSign();
+
+        printColour(105);
+        printColour(50);
+        printColour(-1);
+
+        compareNumbers();
+    }
+
+
+
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = 15;
+        if (a + b >= 0)
+        {
+            System.out.println("сумма положительная");
+        } else {
+            System.out.println("сумма отридцательная");
+        }
+    }
+
+    public static void printColour(int value) {
+        if (value <= 0) {
+            System.out.println("Red");
+        } else if (value <= 100) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
+
+        }
+
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 15;
+        if (a >= b)
+        {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+
+    }
+
+
+}
