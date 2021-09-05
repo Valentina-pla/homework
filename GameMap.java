75819533de359a34ac12fb8eee33bb6063040234
valentina <v.platonova16051991@gmail.com>
2021-09-05 20:33:51 +0300

    lesson7

---
+++ GameMap.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameMap extends JPanel {
+    GameMap() {
+
+        setBackground(new Color(196, 180, 6));
+    }
+}
---
+++ Launcher.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        GameWindow gameWindow=new GameWindow();
+
+
+    }
+
+}
